e5ab58ff8875f7c19b8de818166e4758ee0223c7
yyjim <yy.jim731@gmail.com>
2015-06-16 12:54:21 +0800

    init

---
+++ gpower/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  gpower
+//
+//  Created by yyjim on 6/16/15.
+//  Copyright (c) 2015 cardinalblue. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ gpower/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  gpower
+//
+//  Created by yyjim on 6/16/15.
+//  Copyright (c) 2015 cardinalblue. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
